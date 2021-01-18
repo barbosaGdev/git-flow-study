6e0461a429907dfeb6197a75bf4e47d963aff007
Gabriel Barbosa <32107769+barbosaGdev@users.noreply.github.com>
2021-01-18 12:07:53 -0300

    Revert 3 release/v0 (#4)
    * feature(welcome): welcome to my project
    
    * feature(object): new file (#1)
    
    Co-authored-by: barbosaGdev <barbosagdev@gmail.com>
    
    * Feature/map array (#2)
    
    * feature(object): new file
    
    * feature(array): modify array
    
    Co-authored-by: barbosaGdev <barbosagdev@gmail.com>
    
    * feature(object): new file (#3)
    
    Co-authored-by: barbosaGdev <barbosagdev@gmail.com>
    
    * Revert "feature(object): new file (#3)"
    
    This reverts commit b70e1d92cb9117392640e465f8b5c0695acb70fd.
    
    Co-authored-by: barbosaGdev <barbosagdev@gmail.com>

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log("Hello World!")
+console.log("We going to learn git flow!")
---
+++ object.js
@@ -0,0 +1,5 @@
+const peoples = [{ nome: "Barbosa", linguagem: "Javascript" }, { nome: "Camilo", linguagem: "Ruby" }]
+
+
+peoples.map(item => console.log(item.nome))
+
