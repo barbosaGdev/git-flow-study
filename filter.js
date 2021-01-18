91282f6bf5b5046b6791062b74d041de3e675787
Gabriel Barbosa <32107769+barbosaGdev@users.noreply.github.com>
2021-01-18 15:09:44 -0300

    Release/v2 (#12)
    * feature(object): new file (#3)
    
    Co-authored-by: barbosaGdev <barbosagdev@gmail.com>
    
    * feature operators
    
    * add subtration (#6)
    
    Co-authored-by: barbosaGdev <barbosagdev@gmail.com>
    
    * add multiplication (#7)
    
    Co-authored-by: barbosaGdev <barbosagdev@gmail.com>
    
    * Feature/filter (#9)
    
    * add multiplication
    
    * feature(filter): add filter
    
    Co-authored-by: barbosaGdev <barbosagdev@gmail.com>
    
    * Feature/map (#10)
    
    * add multiplication
    
    * feature(filter): add filter
    
    * add map
    
    Co-authored-by: barbosaGdev <barbosagdev@gmail.com>
    
    * add pop push (#11)
    
    Co-authored-by: barbosaGdev <barbosagdev@gmail.com>
    
    Co-authored-by: barbosaGdev <barbosagdev@gmail.com>

---
+++ filter.js
@@ -0,0 +1,10 @@
+const numbers = [ 1, 2, 3, 4, 5]
+
+const numbersFilter = numbers.filter(item => item > 2)
+
+console.log(numbersFilter)
+
+numbersFilter.map(item => console.log(item))
+
+numbersFilter.push(6)
+numbersFilter.pop()
